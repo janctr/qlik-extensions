8e25354f29cb412be694d9be44b519ce78b83215
janctr <janiverson.eligio.ctr@us.navy.mil>
2023-03-09 09:25:28 -1000

    Add child items.

--- extensions/top-navigation-bar/properties.js
+++ extensions/top-navigation-bar/properties.js
@@ -16,15 +16,110 @@ define(["qlik"], function (qlik) {
       grouped: true,
       itemTitleRef: "label",
       items: {
+        isGroup: {
+          type: "string",
+          component: "radiobuttons",
+          defaultValue: "item",
+          options: [
+            {
+              value: "item",
+              label: "Item",
+            },
+            {
+              value: "item-group",
+              label: "Group",
+            },
+          ],
+          ref: "isGroup",
+          label: "Menu Item Type",
+        },
         label: {
           type: "string",
           ref: "label",
           label: "Label",
         },
+        /******************* Children ******************** */
+        itemGroup: {
+          type: "array",
+          ref: "childMenuItems",
+          show: function (x) {
+            console.log("itemGroup show: ", x);
+
+            return x.isGroup === "item-group";
+          },
+          grouped: true,
+          allowAdd: true,
+          allowRemove: true,
+          allowMove: true,
+          translation: "Menu Items",
+          addTranslation: "Add Item",
+          itemTitleRef: "label",
+          items: {
+            isGroup: {
+              type: "string",
+              component: "radiobuttons",
+              defaultValue: "item",
+              options: [
+                {
+                  value: "item",
+                  label: "Item",
+                },
+                {
+                  value: "item-group",
+                  label: "Group",
+                },
+              ],
+              ref: "isGroup",
+              label: "Menu Item Type",
+            },
+            label: {
+              type: "string",
+              ref: "label",
+              label: "Label",
+            },
+            menuItemType: {
+              type: "string",
+              component: "dropdown",
+              ref: "linkType",
+              label: "Link Type",
+              options: [
+                {
+                  label: "Sheet Link",
+                  value: "sheet-link",
+                },
+                {
+                  label: "Website URL",
+                  value: "web-link",
+                },
+              ],
+            },
+            href: {
+              type: "string",
+              ref: "href",
+              label: "URL",
+              show: function (x) {
+                console.log(x);
+                return x.linkType === "web-link";
+              },
+            },
+            sheet: {
+              type: "string",
+              component: "dropdown",
+              ref: "sheetId",
+              label: "Sheet",
+              defaultValue: 0,
+              options: sheets,
+              show: function (x) {
+                return x.linkType === "sheet-link";
+              },
+            },
+          },
+        },
+        /*******************************************************************/
         menuItemType: {
           type: "string",
           component: "dropdown",
-          ref: "itemType",
+          ref: "linkType",
           label: "Link Type",
           options: [
             {
@@ -43,7 +138,7 @@ define(["qlik"], function (qlik) {
           label: "URL",
           show: function (x) {
             console.log(x);
-            return x.itemType === "web-link";
+            return x.linkType === "web-link";
           },
         },
         sheet: {
@@ -54,30 +149,12 @@ define(["qlik"], function (qlik) {
           defaultValue: 0,
           options: sheets,
           show: function (x) {
-            return x.itemType === "sheet-link";
+            return x.linkType === "sheet-link";
           },
         },
       },
     },
   };
-
-  const itemsDefinitionWithChildren = JSON.parse(
-    JSON.stringify(itemsDefinition)
-  );
-
-  itemsDefinitionWithChildren.menuItems.items.children = {
-    type: "array",
-    translation: "Sub Menu Items",
-    min: 1,
-    allowAdd: true,
-    allowRemove: true,
-    allowMove: true,
-    addTranslation: "Add Sub Menu Item",
-    grouped: true,
-    items: JSON.parse(JSON.stringify(itemsDefinition)),
-  };
-
-  console.log("itemsDefinitionWithChildren: ", itemsDefinitionWithChildren);
 
   const properties = {
     definition: {
