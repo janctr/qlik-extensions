c6a649daf51ab84d40e4a26809dd74936fdf8e58
janctr <janiverson.eligio.ctr@us.navy.mil>
2023-05-12 16:00:05 -1000

    Finalize.

--- extensions/j4-landing-page-link/j4-landing-page-link.js
+++ extensions/j4-landing-page-link/j4-landing-page-link.js
@@ -1,9 +1,19 @@
-define(["qlik", "text!./template.html"], function (qlik, template) {
+define(["qlik", "text!./template.html", "text!./styles.css"], function (
+  qlik,
+  template,
+  styles
+) {
+  "use strict";
+
+  [styles].forEach((cssModule) => {
+    $("<style>").html(cssModule).appendTo("head");
+  });
+
   return {
     template: template,
     support: {
-      snapshot: true,
-      export: true,
+      snapshot: false,
+      export: false,
       exportData: false,
     },
     paint: function () {
@@ -12,8 +22,30 @@ define(["qlik", "text!./template.html"], function (qlik, template) {
     controller: [
       "$scope",
       function ($scope) {
-        //add your rendering code here
-        $scope.html = "Hello World";
+        $scope.j4Link =
+          "https://qlik.advana.data.mil/sense/app/0695026d-95cb-40bc-8db3-f19e944f9994/sheet/91d194c4-60a3-4186-a6ac-5c4a7042ac71/state/edit";
+        $scope.isToggled = false;
+
+        $(document).ready(function () {
+          $(".j4-landing-page-link-container").hover(function (event) {
+            event.preventDefault();
+            event.stopPropagation();
+          });
+          $(".j4-minimize").hover(function (event) {
+            event.preventDefault();
+            event.stopPropagation();
+          });
+          $(".j4-minimize").click(function () {
+            $(this).toggleClass("toggled");
+            $scope.isToggled = !$scope.isToggled;
+
+            if ($scope.isToggled) {
+              $(".j4-landing-page-link-container").css("bottom", "-20px");
+            } else {
+              $(".j4-landing-page-link-container").removeAttr("style");
+            }
+          });
+        });
       },
     ],
   };
