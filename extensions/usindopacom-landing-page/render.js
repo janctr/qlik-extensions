d5b591f305edd4232507220d0a1a544a299ffe1d
janctr <janiverson.eligio.ctr@us.navy.mil>
2023-05-22 09:53:52 -1000

    Refactor render code.

---
+++ extensions/usindopacom-landing-page/render.js
@@ -0,0 +1,159 @@
+define(["jquery", "./util"], function ($, Util) {
+  const {
+    appId,
+    sheets,
+    isWebLink,
+    isSheetLink,
+    isNotLink,
+    getSheetUrl,
+    getHref,
+    getBackgroundImageUrl,
+    makeClassFromTitle,
+    navigateToSheet,
+    navigateToUrl,
+    navigateToUrlInNewTab,
+  } = Util;
+
+  function render(layout) {
+    const isSipr = layout.pageSettings.isSipr;
+    const pageTitleBackgroundColor =
+      layout.pageSettings.pageTitleBackgroundColor.color;
+    const pageTitleTextColor = layout.pageSettings.pageTitleTextColor.color;
+
+    $(".j43-card-wrapper").css(
+      "background-color",
+      layout.pageSettings.pageBackgroundColor.color
+    );
+
+    for (const menuItem of layout.menuItems) {
+      $("<style>").html(menuItem.customCss).appendTo("head");
+
+      const cardClass = menuItem.cardClass || makeClassFromTitle(menuItem);
+
+      if (cardClass) {
+        /* Apply custom background image */
+        if (menuItem.coverImageUrl) {
+          $(`.${menuItem.cardClass} > .front`).css(
+            "background-image",
+            getBackgroundImageUrl({ menuItem })
+          );
+        }
+
+        if (menuItem.isFlippable) {
+          /* Toggle hover class for cards on hover */
+          $(`.container.${cardClass}`).hover(function () {
+            $(this).toggleClass("hover");
+          });
+          /* Make it so when you click on a card it stays flipped */
+          $(`.container.${cardClass}`).click(function () {
+            $(this).toggleClass("keep-hovered");
+          });
+        }
+
+        if (
+          menuItem.clickToFollowLink &&
+          !isNotLink(menuItem) &&
+          (menuItem.href || menuItem.sheetId)
+        ) {
+          /* Determine the onClick callback */
+          let handler;
+          if (menuItem.sheetId && isSheetLink(menuItem)) {
+            handler = function () {
+              navigateToSheet(menuItem.sheetId);
+            };
+          } else {
+            handler = function () {
+              navigateToUrlInNewTab(
+                getHref({
+                  menuItem,
+                  isSipr,
+                })
+              );
+            };
+          }
+
+          /* Assign onClick event handler */
+          $(`.${cardClass}`).parent().click(handler);
+        } else if (
+          !isNotLink(menuItem) &&
+          (menuItem.href || menuItem.sheetId)
+        ) {
+          if (menuItem.sheetId && isSheetLink(menuItem)) {
+            $(`.${menuItem.cardClass} > .back > a`).click(function () {
+              navigateToSheet(menuItem.sheetId);
+            });
+          } else {
+            $(`.${menuItem.cardClass} > .back > a`).attr(
+              "href",
+              getHref({
+                menuItem,
+                isSipr,
+              })
+            );
+          }
+        } else {
+          // No link for this card
+        }
+
+        /* Make font smaller if too much text */
+        if (menuItem.isComingSoon) {
+          if (menuItem.ribbonLabel && menuItem.ribbonLabel.length >= 14) {
+            $(`.${menuItem.cardClass} .coming-soon-ribbon`).toggleClass(
+              "font-smaller"
+            );
+          }
+        }
+      }
+
+      /* Apply custom front/back styles */
+      if (menuItem.cardFrontStyles) {
+        const cardFrontEl = $(`.${menuItem.cardClass} > .front`);
+        cardFrontEl.attr(
+          "style",
+          cardFrontEl.attr("style") + ";" + menuItem.cardFrontStyles
+        );
+      }
+
+      if (menuItem.cardBackStyles) {
+        const cardBackEl = $(`.${menuItem.cardClass} > .back`);
+        cardBackEl.attr(
+          "style",
+          cardBackEl.attr("style") + ";" + menuItem.cardBackStyles
+        );
+      }
+    }
+
+    /* Apply easter egg */
+    $(".jloc > .back").append(
+      $(
+        `<div 
+              class="easter-egg"
+              style="
+              height:100%;
+              width:100%; 
+              position: absolute;
+              background-image: url('/appcontent/${appId}/kobe.jpg');"></div>`
+      )
+    );
+
+    $(".jloc > .back").on("keyup", function (event) {
+      if (event.keyCode === 56) {
+        $(".easter-egg").css("top", 0);
+        $(".easter-egg").css("opacity", 1);
+      }
+    });
+    /********************/
+
+    if (pageTitleBackgroundColor) {
+      $(".j43-page-title").css("background-color", pageTitleBackgroundColor);
+    }
+
+    if (pageTitleTextColor) {
+      $(".j43-page-title").css("color", pageTitleTextColor);
+    }
+  }
+
+  return {
+    render,
+  };
+});
--- extensions/usindopacom-landing-page/usindopacom-landing-page.js
+++ extensions/usindopacom-landing-page/usindopacom-landing-page.js
@@ -3,12 +3,23 @@ define([
   "jquery",
   "./properties",
   "./util",
+  "./render",
   "text!./style.css",
   "text!./parallax.css",
   "text!./index.html",
-], function (qlik, $, properties, Util, cssContent, parallaxCss, template) {
+], function (
+  qlik,
+  $,
+  properties,
+  Util,
+  Render,
+  cssContent,
+  parallaxCss,
+  template
+) {
   "use strict";
 
+  const { render } = Render;
   const {
     appId,
     sheets,
@@ -49,6 +60,7 @@ define([
       exportData: true,
     },
     paint: function ($element, layout) {
+      render(layout);
       return qlik.Promise.resolve();
     },
     controller: [
@@ -71,142 +83,8 @@ define([
         $scope.makeClassFromTitle = makeClassFromTitle;
 
         $(document).ready(() => {
-          $(".j43-card-wrapper").css(
-            "background-color",
-            layout.pageSettings.pageBackgroundColor.color
-          );
-
-          for (const menuItem of layout.menuItems) {
-            $("<style>").html(menuItem.customCss).appendTo("head");
-
-            const cardClass =
-              menuItem.cardClass || makeClassFromTitle(menuItem);
-
-            if (cardClass) {
-              /* Apply custom background image */
-              if (menuItem.coverImageUrl) {
-                $(`.${menuItem.cardClass} > .front`).css(
-                  "background-image",
-                  getBackgroundImageUrl({ menuItem })
-                );
-              }
-
-              if (menuItem.isFlippable) {
-                /* Toggle hover class for cards on hover */
-                $(`.container.${cardClass}`).hover(function () {
-                  $(this).toggleClass("hover");
-                });
-                /* Make it so when you click on a card it stays flipped */
-                $(`.container.${cardClass}`).click(function () {
-                  $(this).toggleClass("keep-hovered");
-                });
-              }
-
-              if (
-                menuItem.clickToFollowLink &&
-                !isNotLink(menuItem) &&
-                (menuItem.href || menuItem.sheetId)
-              ) {
-                /* Determine the onClick callback */
-                let handler;
-                if (menuItem.sheetId && isSheetLink(menuItem)) {
-                  handler = function () {
-                    navigateToSheet(menuItem.sheetId);
-                  };
-                } else {
-                  handler = function () {
-                    navigateToUrlInNewTab(
-                      getHref({
-                        menuItem,
-                        isSipr: $scope.isSipr,
-                      })
-                    );
-                  };
-                }
-
-                /* Assign onClick event handler */
-                $(`.${cardClass}`).parent().click(handler);
-              } else if (
-                !isNotLink(menuItem) &&
-                (menuItem.href || menuItem.sheetId)
-              ) {
-                if (menuItem.sheetId && isSheetLink(menuItem)) {
-                  $(`.${menuItem.cardClass} > .back > a`).click(function () {
-                    navigateToSheet(menuItem.sheetId);
-                  });
-                } else {
-                  $(`.${menuItem.cardClass} > .back > a`).attr(
-                    "href",
-                    getHref({
-                      menuItem,
-                      isSipr: $scope.isSipr,
-                    })
-                  );
-                }
-              } else {
-                // No link for this card
-              }
-
-              /* Make font smaller if too much text */
-              if (menuItem.isComingSoon) {
-                if (menuItem.ribbonLabel && menuItem.ribbonLabel.length >= 14) {
-                  $(`.${menuItem.cardClass} .coming-soon-ribbon`).toggleClass(
-                    "font-smaller"
-                  );
-                }
-              }
-            }
-
-            /* Apply custom front/back styles */
-            if (menuItem.cardFrontStyles) {
-              const cardFrontEl = $(`.${menuItem.cardClass} > .front`);
-              cardFrontEl.attr(
-                "style",
-                cardFrontEl.attr("style") + ";" + menuItem.cardFrontStyles
-              );
-            }
-
-            if (menuItem.cardBackStyles) {
-              const cardBackEl = $(`.${menuItem.cardClass} > .back`);
-              cardBackEl.attr(
-                "style",
-                cardBackEl.attr("style") + ";" + menuItem.cardBackStyles
-              );
-            }
-          }
-
-          /* Apply easter egg */
-          $(".jloc > .back").append(
-            $(
-              `<div 
-                class="easter-egg"
-                style="
-                height:100%;
-                width:100%; 
-                position: absolute;
-                background-image: url('/appcontent/${appId}/kobe.jpg');"></div>`
-            )
-          );
-
-          $(".jloc > .back").on("keyup", function (event) {
-            if (event.keyCode === 56) {
-              $(".easter-egg").css("top", 0);
-              $(".easter-egg").css("opacity", 1);
-            }
-          });
-          /********************/
+          render(layout);
         });
-
-        if ($scope.pageTitleBackgroundColor) {
-          $(".j43-page-title").css(
-            "background-color",
-            $scope.pageTitleBackgroundColor
-          );
-        }
-
-        if ($scope.pageTitleTextColor) {
-          $(".j43-page-title").css("color", $scope.pageTitleTextColor);
-        }
       },
     ],
   };
